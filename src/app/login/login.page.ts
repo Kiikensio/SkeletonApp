ef595dd9d5399a0b14de94e6a37ad09c36657011
Enrique Catalan <enr.catalan@duocuc.cl>
2023-10-02 13:45:46 -0300

    add requerimientos de user y password

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -10,14 +10,17 @@ export class LoginPage implements OnInit {
 
   constructor(private router: Router) {}
 
-  
-
+  inputValue: string = '';
   horaActual: string = '';
+  maskedPassword: string = '';
+  password: string = '';
 
   ngOnInit() {
     this.actualizarHora();
     setInterval(() => this.actualizarHora(), 1000); // Actualiza la hora cada segundo
   }
+
+  
 
   actualizarHora() {
     const ahora = new Date();
@@ -28,4 +31,14 @@ export class LoginPage implements OnInit {
     this.router.navigate(['home']);
   }
 
+  maskPassword() {
+    // Oculta la contraseña mostrando "****" en lugar de los caracteres reales.
+    this.maskedPassword = '*'.repeat(this.password.length);
+  }
+
+  submitForm() {
+    // Aquí puedes realizar acciones con el valor ingresado, como enviarlo a un servidor o procesarlo de alguna manera.
+    console.log('Valor ingresado:', this.inputValue);
+    console.log('Contraseña ingresada:', this.password);
+  }
 }
