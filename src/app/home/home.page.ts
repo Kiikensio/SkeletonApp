37438147c1d86902bb8e688e79b7ce85951daa72
Enrique Catalan <enr.catalan@duocuc.cl>
2023-10-10 23:23:12 -0300

    ad

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  horaActual: string = '';
+
+  ngOnInit() {
+    this.actualizarHora();
+    setInterval(() => this.actualizarHora(), 1000); // Actualiza la hora cada segundo
+  }
+
+  actualizarHora() {
+    const ahora = new Date();
+    this.horaActual = ahora.toLocaleTimeString();
+  }
 
 }
